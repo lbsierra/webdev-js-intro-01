36a33b3f5087f4f45e853edd7f8af94d3145b468
Sierra <sonicaradia@gmail.com>
2024-12-19 11:57:40 -0500

    Did part one JS experiment

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/* 
+const eight = 8;
+console.log(eight);
+
+let dinner = "Spaghetti Carbonara";
+console.log(dinner);
+
+dinner ="Tacos";
+console.log(dinner); 
+*/
